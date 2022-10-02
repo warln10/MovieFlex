80a3264f7f8fc526909d4774b13c94d42f5be562
warln10 <tyagiabhishek99@hotmail.com>
2022-10-02 19:47:38 +0530

    add new conrtoller

---
+++ MovieFlex/Controller/General/testVc1.swift
@@ -0,0 +1,29 @@
+//
+//  testVc1.swift
+//  MovieFlex
+//
+//  Created by Warln on 02/10/22.
+//
+
+import UIKit
+
+class testVc1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
